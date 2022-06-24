cc0cbc6031a7f46441f25ae17ec164977d1aafb7
ys-tech-z <y.s.moko.1214@gmail.com>
2022-06-24 20:37:26 +0900

    2回目です

--- app/src/main/java/jp/techacademy/yuki/sakamaki/humanclass/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/sakamaki/humanclass/MainActivity.kt
@@ -12,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         val hito = Human("酒巻",12,"世界")
         hito.say()
         hito.think()
+
+        val hito2 = Human("佐藤",15,"パズル")
+        hito2.say()
+        hito2.think()
     }
 }
